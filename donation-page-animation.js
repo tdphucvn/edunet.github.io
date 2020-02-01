72dc6a7a5edfdd82b03f871670195e6963a3ab64
Philip Tran <tran.philip02@gmail.com>
2020-02-01 22:25:20 +0100

    Added new files

--- donation-page-animation.js
+++ donation-page-animation.js
@@ -1,12 +1,13 @@
-//const hero = document.querySelector('.donate-container');
+const donate1 = document.querySelector('.donate-1');
+const donate2 = document.querySelector('.donate-2');
+const donate3 = document.querySelector('.donate-3');
 const slider = document.querySelector('.slider-4');
-const hamburger = document.querySelector('.hamburger');
-const nav = document.querySelector('.main-nav');
 const logo = document.querySelector('.main-nav-item-1');
-const headline = document.querySelector('.headline');
 
 const tl=new TimelineLite();
 
 tl.fromTo(slider, 1.2, {x: "-100%"}, {x:"0%", ease: Power2.easeInOut})
 .fromTo(logo, 0.5, {opacity:0, x:150}, {opacity:1, x:0}, "-=0.5")
-.fromTo(headline, 0.5, {opacity:0, x:150}, {opacity:1, x:0}, "-=0.5")
+.fromTo(donate1, 1.5,{x: "120%"}, {x:"0%", ease: Power2.easeInOut}, "-=0.7")
+.fromTo(donate2, 1.5,{x: "120%"}, {x:"0%", ease: Power2.easeInOut}, "-=0.9")
+.fromTo(donate3, 1.5,{x: "120%"}, {x:"0%", ease: Power2.easeInOut}, "-=0.9")
--- matematika/matematika-animation.js
+++ matematika/matematika-animation.js
@@ -3,10 +3,8 @@ const slider = document.querySelector('.slider-3');
 const hamburger = document.querySelector('.hamburger');
 const nav = document.querySelector('.main-nav');
 const logo = document.querySelector('.main-nav-item-1');
-const listheader = document.querySelectorAll('.list-header');
 
 const tl=new TimelineLite();
 
 tl.fromTo(slider, 1.2, {x: "-100%"}, {x:"0%", ease: Power2.easeInOut})
 .fromTo(logo, 0.5, {opacity:0, x:150}, {opacity:1, x:0}, "-=1.2")
-.fromTo(listheader, 1.5, {x: "-100%"}, {x:"0%", ease: Power2.easeInOut},"-=1.2")
